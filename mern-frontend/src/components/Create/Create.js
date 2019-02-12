2e1218e9e6bd5413bdf1170e435e7a00cef4abf8
Pierson Brannan <plbrannan95@gmail.com>
2019-02-12 14:52:30 -0500

    Cretae work

--- mern-frontend/src/components/Create/Create.js
+++ mern-frontend/src/components/Create/Create.js
@@ -1,44 +1,57 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import axios from "axios";
+import { EACCES } from "constants";
 
 class Create extends Component {
-	constructor() {
-		super()
-		this.state = {
-			onePost: [],
-      title: "",
-      content: ""
-		}
-	}
+   constructor() {
+      super();
+      this.state = {
+         onePost: [],
+         title: "",
+         content: ""
+      };
+   }
 
-	handleSubmit(e) {
-    const url = "http://localhost:3001/create";
-    // const soloPost = this.props.match.params._id;
-    e.preventDefault();
-    axios
-      .put(`${url}`, {
-        title: this.state.title,
-        content: this.state.content
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  }
-	render() {
-		return (
-			<div>
-				<form onSubmit={this.handleSubmit}>
-      	<div className="form">
-        <input name="title" type="text" value={this.state.title} placeholder="Title" />
-				<br/>
-        <input name="content" type="text" value={this.state.content} placeholder="Content" />
-				<br/>
-        <input type="submit" value="Submit" />
-      </div>
-      </form>
-			</div>
-		);
-	}
+   handleSubmit(e) {
+       console.log("Handle submit pressed")
+      const url = "http://localhost:3001/create/";
+      // const soloPost = this.props.match.params._id;
+      e.preventDefault();
+      console.log(e.target.title.value)
+      axios
+         .post(`${url}`, {
+            title: e.target.title.value,
+            content: e.target.content.value
+         })
+         .catch(err => {
+            console.log(err);
+         });
+   }
+   render() {
+      return (
+         <div>
+            <form onSubmit={this.handleSubmit}>
+               <div className="form">
+                  <input
+                     name="title"
+                     type="text"
+                    //  value={this.state.title}
+                     placeholder="Title"
+                  />
+                  <br />
+                  <input
+                     name="content"
+                     type="text"
+                    //  value={this.state.content}
+                     placeholder="Content"
+                  />
+                  <br />
+                  <input type="submit" value="Submit" />
+               </div>
+            </form>
+         </div>
+      );
+   }
 }
 
 export default Create;
