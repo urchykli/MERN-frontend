8d7d832494560114798b6c2e28d5d0c4e8139b21
Lindsay Urchyk <urchykli@gmail.com>
2019-02-12 13:58:50 -0500

    delete isn't working

--- mern-frontend/src/components/Create/Create.js
+++ mern-frontend/src/components/Create/Create.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
 
 class Create extends Component {
+	constructor() {
+		super()
+		this.state = {
+
+		}
+		this.handleChange = this.handleChange.bind(this)
+	}
+	handleChange(e) {
+    // console.log(name, value)
+    this.setState({[e.target.name]: e.target.value})
+  }
 	render() {
 		return (
 			<div>
-				
+				<form onSubmit={this.handleSubmit}>
+      	<div className="form">
+        <input name="title" type="text" value={this.state.title} onChange={this.handleChange} placeholder="Title" />
+				<br/>
+        <input name="content" type="text" value={this.state.content} onChange={this.handleChange} placeholder="Content" />
+				<br/>
+        <input type="submit" value="Submit" />
+      </div>
+      </form>
 			</div>
 		);
 	}
--- mern-frontend/src/components/Show/Show.js
+++ mern-frontend/src/components/Show/Show.js
@@ -1,72 +1,92 @@
-import React, { Component } from 'react';
-import axios from 'axios'
+import React, { Component } from "react";
+import axios from "axios";
 
 class Show extends Component {
-	constructor (props) {
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
       onePost: [],
-      title: '',
-      content: ''
-
-    }
-    this.handleChange = this.handleChange.bind(this)
+      title: "",
+      content: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-	}
-	componentDidMount () {
-		const url = 'http://localhost:3001/show'
-		const soloPost = this.props.match.params._id
-		console.log(soloPost)
-    axios.get(`${url}/${soloPost}`)
-		.then((res) => {
-			console.log(res)
-			this.setState({
-				onePost: res.data
-			})
-    })
-      .catch((err) => {
-        console.log(err)
+    this.handleDestroy = this.handleDestroy.bind(this);
+  }
+  componentDidMount() {
+    const url = "http://localhost:3001/show";
+    const soloPost = this.props.match.params._id;
+    console.log(soloPost);
+    axios
+      .get(`${url}/${soloPost}`)
+      .then(res => {
+        console.log(res);
+        this.setState({
+          onePost: res.data
+        });
       })
+      .catch(err => {
+        console.log(err);
+      });
   }
   handleChange(e) {
     // console.log(name, value)
-    this.setState({[e.target.name]: e.target.value})
+    this.setState({ [e.target.name]: e.target.value });
   }
-  handleSubmit (e) {
-    console.log('handleSubmit called')
-    const url = 'http://localhost:3001/show'
-    const soloPost = this.props.match.params._id
-    e.preventDefault()
-    axios.put(`${url}/${soloPost}`, {
-      title: this.state.title,
-      content: this.state.content
-    })
-    .catch((err) => {
-      console.log(err)
-    })
+  handleSubmit(e) {
+    const url = "http://localhost:3001/show";
+    const soloPost = this.props.match.params._id;
+    e.preventDefault();
+    axios
+      .put(`${url}/${soloPost}`, {
+        title: this.state.title,
+        content: this.state.content
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
-	render() {
-		console.log("show rendered")
-    console.log(this.state.onePost)
-    console.log(this.state.title)
-		return (
-			<div>
-				<h1>{this.state.onePost.title}</h1>
-				<p>{this.state.onePost.content}</p>
-				<h4>{this.state.onePost.createdAt}</h4>
-				<form onSubmit={this.handleSubmit}>
-      	<div className="form">
-        <input name="title" type="text" value={this.state.title} onChange={this.handleChange} placeholder="Title" />
-				<br/>
-        <input name="content" type="text" value={this.state.content} onChange={this.handleChange} placeholder="Content" />
-				<br/>
-        <input type="submit" value="Update" />
+  handleDestroy(e) {
+    // const url = "http://localhost:3001/show";
+    // const soloPost = this.props.match.params._id;
+    axios.delete("http://localhost:3001/show", { params: { id: e } });
+  }
+  render() {
+    console.log("show rendered");
+    console.log(this.state.onePost);
+    console.log(this.state.title);
+    return (
+      <div>
+        <h1>{this.state.onePost.title}</h1>
+        <p>{this.state.onePost.content}</p>
+        <h4>{this.state.onePost.createdAt}</h4>
+        <form onSubmit={this.handleSubmit}>
+          <div className="form">
+            <input
+              name="title"
+              type="text"
+              value={this.state.title}
+              onChange={this.handleChange}
+              placeholder="Title"
+            />
+            <br />
+            <input
+              name="content"
+              type="text"
+              value={this.state.content}
+              onChange={this.handleChange}
+              placeholder="Content"
+            />
+            <br />
+            <input type="submit" value="Update" />
+          </div>
+        </form>
+        <form method="delete">
+        <input type="submit" value="Delete" onClick={this.handleDestroy(this)} />
+        </form>
       </div>
-      </form>
-			</div>
-		);
-	}
+    );
+  }
 }
-
 
 export default Show;
